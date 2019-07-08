58afe514c9d8014b72884ef1a71d2461da310672
黄瑞锋 <huangruifeng@hqjy.com>
2019-07-08 17:59:07 +0800

    测试提交

--- danmu/src/main/java/com/ricky/danmu/DanmuApplication.java
+++ danmu/src/main/java/com/ricky/danmu/DanmuApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DanmuApplication {
-
+    //??
     public static void main(String[] args) {
         SpringApplication.run(DanmuApplication.class, args);
     }
